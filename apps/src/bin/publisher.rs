7e7c949bd0883edc14502e8bfd17ab91f1b1c147
Rahul Tripathi <rahultripathidev@gmail.com>
2024-04-20 10:06:55 +0530

    ft: add state root

--- apps/src/bin/publisher.rs
+++ apps/src/bin/publisher.rs
@@ -9,35 +9,35 @@ use ethers_core::types::Address;
 use ethers_signers::{LocalWallet, Signer};
 use futures;
 use futures::executor::block_on;
-use hexutil::to_hex;
+use hexutil::{to_hex, read_hex};
 
 use apps::{BonsaiProver, EOASignature, Z0Req};
 use methods::Z0_GENERATOR_ELF;
 
-async fn gen_test_input()  -> Z0Req {
-    let message_hash = keccak256("hello".as_bytes());
+async fn gen_test_input() -> Z0Req {
+    let message_hash = read_hex("0x8649d736c2af537facc35382d7e0d8503ed5f036fa5201a9186da1d4db189640").unwrap();
     let wallet: LocalWallet = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7"
         .parse::<LocalWallet>()
         .unwrap();
     let signature = wallet.sign_message(to_hex(message_hash.as_slice()).as_str()).await.unwrap();
     let threshold = U256::from_str("1").unwrap().encode();
-    let nonce  = U256::from_str("0").unwrap().encode();
-    return Z0Req{
+    let nonce = U256::from_str("0").unwrap().encode();
+    return Z0Req {
         signers: vec![H160::from_str("0x63f9725f107358c9115bc9d86c72dd5823e9b1e6").unwrap() as Address, H160::from_str("0x687f4304Df62449dBc6C95FE9A8cb1153d40D42e").unwrap() as Address, H160::from_str("0x0f8361eF429B43fA48aC66A7cD8F619C517274f1").unwrap() as Address],
         threshold: to_hex(threshold.as_slice()),
         nonce: to_hex(nonce.as_slice()),
-        message_hash:to_hex(message_hash.as_slice()),
-        signatures: vec![EOASignature{
+        message_hash: to_hex(message_hash.as_slice()),
+        signatures: vec![EOASignature {
             eoa: wallet.address(),
             signature: to_hex(signature.to_vec().as_slice()),
         }],
-    }
+    };
 }
 
 fn main() -> Result<()> {
     env_logger::init();
 
-    let input  = block_on(gen_test_input());
+    let input = block_on(gen_test_input());
     // Send an off-chain proof request to the Bonsai proving service.
     let (journal, post_state_digest, seal) = BonsaiProver::prove(Z0_GENERATOR_ELF, &input)?;
     let digest = post_state_digest.as_slice();
